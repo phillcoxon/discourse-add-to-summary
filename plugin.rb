892e8dd15092e3e9449f2e49ea7c813af9c2e86d
Jay Pfaffman <pfaffman@gmail.com>
2018-12-30 15:55:20 -0600

    allow plugin to be disabled & remove class

--- plugin.rb
+++ plugin.rb
@@ -13,73 +13,75 @@ PLUGIN_NAME ||= "DiscourseAddToSummary".freeze
 
 Rails.configuration.paths['app/views'].unshift(Rails.root.join('plugins', 'discourse-add-to-summary', 'app/views'))
 
-after_initialize do
+if enabled_site_setting
+  after_initialize do
 
-  @my_data = {}
-  @my_data[:test] = "this is my text which I have given unto you from the start."
+    # see lib/plugin/instance.rb for the methods available in this context
 
-
-  # see lib/plugin/instance.rb for the methods available in this context
-
-  module ::DiscourseAddToSummary
-    class Engine < ::Rails::Engine
-      engine_name PLUGIN_NAME
-      #      isolate_namespace DiscourseAddToSummary
-    end
-  end
-
-  module AddToMailerExtension
-    def digest(user, opts = {})
-      @add_to_data = {}
-      base = Discourse.base_url
-      base.gsub!(/localhost/,"localhost:3000") # make this work for development
-      before_id=SiteSetting.discourse_add_to_summary_before_header_topic_id
-      before_post_list = Post.where(topic_id: before_id, post_number: 1)
-      before_text = ""
-      if before_post_list.length > 0
-        before_text = before_post_list.first.cooked
+    module ::DiscourseAddToSummary
+      class Engine < ::Rails::Engine
+        engine_name PLUGIN_NAME
+        #      isolate_namespace DiscourseAddToSummary
       end
-      before_text.gsub!(/\/\/localhost/,base)
-      @add_to_data[:before_text] = before_text
-      @add_to_data[:before_css] = SiteSetting.discourse_add_to_summary_before_header_css
-      @add_to_data[:before] = before_text.length>0
+    end
 
-      after_id=SiteSetting.discourse_add_to_summary_after_header_topic_id
-      after_post_list = Post.where(topic_id: after_id, post_number: 1)
-      after_text = ""
-      if after_post_list.length > 0
-        after_text = after_post_list.first.cooked
+    module AddToMailerExtension
+      def digest(user, opts = {})
+        @add_to_data = {}
+        if SiteSetting.discourse_add_to_summary_enabled
+          @add_to_data = {}
+          base = Discourse.base_url
+          base.gsub!(/localhost/,"localhost:3000") # make this work for development
+          before_id=SiteSetting.discourse_add_to_summary_before_header_topic_id
+          before_post_list = Post.where(topic_id: before_id, post_number: 1)
+          before_text = ""
+          if before_post_list.length > 0
+            before_text = before_post_list.first.cooked
+          end
+          before_text.gsub!(/\/\/localhost/,base)
+          @add_to_data[:before_text] = before_text
+          @add_to_data[:before_css] = SiteSetting.discourse_add_to_summary_before_header_css
+          @add_to_data[:before] = before_text.length>0
+
+          after_id=SiteSetting.discourse_add_to_summary_after_header_topic_id
+          after_post_list = Post.where(topic_id: after_id, post_number: 1)
+          after_text = ""
+          if after_post_list.length > 0
+            after_text = after_post_list.first.cooked
+          end
+          after_text.gsub!(/\/\/localhost/,base)
+          @add_to_data[:after_text] = after_text
+          @add_to_data[:after_css] = SiteSetting.discourse_add_to_summary_after_header_css
+          @add_to_data[:after] = after_text.length>0
+        end
+        super(user, opts)
       end
-      after_text.gsub!(/\/\/localhost/,base)
-      @add_to_data[:after_text] = after_text
-      @add_to_data[:after_css] = SiteSetting.discourse_add_to_summary_after_header_css
-      @add_to_data[:after] = after_text.length>0
-
-      super(user, opts)
     end
-  end
 
-  require_dependency 'user_notifications'
-  class ::UserNotifications
-    prepend AddToMailerExtension
-  end
-
-  require_dependency "application_controller"
-  class DiscourseAddToSummary::ActionsController < ::ApplicationController
-    requires_plugin PLUGIN_NAME
-
-    before_action :ensure_logged_in
-
-    def list
-      render json: success_json
+    require_dependency 'user_notifications'
+    class ::UserNotifications
+      if SiteSetting.discourse_add_to_summary_enabled
+        prepend AddToMailerExtension
+      end
     end
-  end
 
-  DiscourseAddToSummary::Engine.routes.draw do
-    get "/list" => "actions#list"
-  end
+    require_dependency "application_controller"
+    class DiscourseAddToSummary::ActionsController < ::ApplicationController
+      requires_plugin PLUGIN_NAME
 
-  Discourse::Application.routes.append do
-    mount ::DiscourseAddToSummary::Engine, at: "/discourse-add-to-summary"
+      before_action :ensure_logged_in
+
+      def list
+        render json: success_json
+      end
+    end
+
+    DiscourseAddToSummary::Engine.routes.draw do
+      get "/list" => "actions#list"
+    end
+
+    Discourse::Application.routes.append do
+      mount ::DiscourseAddToSummary::Engine, at: "/discourse-add-to-summary"
+    end
   end
 end
